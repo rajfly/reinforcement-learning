f7192c95ad84d9126d071b1196f3f5723c3628f7
rajdeephu <rajdeephu@gmail.com>
2022-11-23 19:15:41 +0800

    minor

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 rm -rf default
-# for env in CartPole-v1 Taxi-v3; do
-for env in CartPole-v1; do
-    # A2C A3C APPO ARS BC CQL DDPG APEX-DDPG ES DQN Rainbow APEX-DQN IMPALA MAML MARWIL PG PPO R2D2 SAC SlateQ TD3
-    for algo in A2C A3C; do
-        # echo "Environment: $env Algorithm: $algo"
-        time rllib train --run $algo --env $env --local-dir "~/projects/reinforcement-learning" --config '{"num_gpus": 1, "framework": {"grid_search": ["tf", "tfe", "tf2", "torch"]}}' &
+
+# for env in CartPole-v1; do
+#     for algo in DQN; do
+#         rllib train --run $algo --env $env --checkpoint-at-end --stop '{"timesteps_total": 300000000}' --local-dir "~/projects/reinforcement-learning" --config '{"num_gpus": 1, "num_workers": 15, "num_envs_per_worker": 200, "framework": {"grid_search": ["tf", "tfe", "tf2", "torch"]}}'
+#     done
+# done
+
+# https://paperswithcode.com/sota/atari-games-on-atari-2600-montezumas-revenge
+for env in ALE/MontezumaRevenge-v5; do
+    for algo in APEX; do
+        rllib train --run $algo --env $env --checkpoint-at-end --stop '{"timesteps_total": 300000000}' --local-dir "~/projects/reinforcement-learning" --config '{"num_gpus": 1, "num_workers": 15, "num_envs_per_worker": 200, "framework": {"grid_search": ["tf", "tfe", "tf2", "torch"]}}'
     done
 done
