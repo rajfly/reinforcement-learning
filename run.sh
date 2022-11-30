44630bce6e44c1cecd4018a8c1a284f1846c8d24
rajdeephu <rajdeephu@gmail.com>
2022-11-30 10:46:16 +0700

    minor

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rllib train -f experiments/pendulum/dqn.yaml
-rllib train -f experiments/pendulum/a2c.yaml
-rllib train -f experiments/pendulum/a3c.yaml
-rllib train -f experiments/pendulum/ppo.yaml
+rllib train -f config/pendulum/dqn.yaml
+rllib train -f config/pendulum/a2c.yaml
+rllib train -f config/pendulum/a3c.yaml
+rllib train -f config/pendulum/ppo.yaml
