4f38928ab72341a802175ae0542a5c36a15c1506
rajdeephu <rajdeephu@gmail.com>
2022-11-30 20:18:10 +0800

    added taxi

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-rllib train -f config/mountain_car/dqn.yaml
-rllib train -f config/mountain_car/a2c.yaml
-rllib train -f config/mountain_car/ppo.yaml
+# rllib train -f config/mountain_car/dqn.yaml
+# rllib train -f config/mountain_car/a2c.yaml
+# rllib train -f config/mountain_car/ppo.yaml
+
+rllib train -f config/breakout/dqn.yaml
+# rllib train -f config/breakout/a2c.yaml
+# rllib train -f config/breakout/ppo.yaml
