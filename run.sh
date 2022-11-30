72eff4c0c43f619ecf51e2d82aa574e788d58e07
rajdeephu <rajdeephu@gmail.com>
2022-11-30 20:31:15 +0800

    minor

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # rllib train -f config/mountain_car/a2c.yaml
 # rllib train -f config/mountain_car/ppo.yaml
 
-rllib train -f config/breakout/dqn.yaml
-# rllib train -f config/breakout/a2c.yaml
-# rllib train -f config/breakout/ppo.yaml
+rllib train -f config/taxi/dqn.yaml
+rllib train -f config/taxi/a2c.yaml
+rllib train -f config/taxi/ppo.yaml
