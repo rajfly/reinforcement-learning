da70f92bf7cc76622e618762c2c5f1f54ed8107e
rajdeephu <rajdeephu@gmail.com>
2022-12-01 18:24:38 +0800

    added appo, ars, pg configs

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # rllib train -f config/cartpole/impala.yaml
 # rllib train -f config/cartpole/apex.yaml
-rllib train -f config/cartpole/sac.yaml
 
-# rllib train -f config/cartpole/dqn.yaml
-# rllib train -f config/cartpole/a2c.yaml
-# rllib train -f config/cartpole/ppo.yaml
+rllib train -f config/cartpole/dqn.yaml
+rllib train -f config/cartpole/a2c.yaml
+rllib train -f config/cartpole/ppo.yaml
+
+# train sac for 5m instead of 10m (estimated is 16 hour for 10m)
+# rllib train -f config/cartpole/appo.yaml
+# rllib train -f config/cartpole/pg.yaml
+# rllib train -f config/cartpole/ars.yaml
+# rllib train -f config/cartpole/sac.yaml
