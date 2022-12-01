b4cb32add91b98a6545eae26752c0d550428c720
rajdeephu <rajdeephu@gmail.com>
2022-12-01 13:35:33 +0800

    minor

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-rllib train -f config/mountain_car/dqn.yaml
-rllib train -f config/mountain_car/ppo.yaml
-rllib train -f config/mountain_car/a2c.yaml
-
-# rllib train -f config/taxi/dqn.yaml
-# rllib train -f config/taxi/a2c.yaml
-# rllib train -f config/taxi/ppo.yaml
+rllib train -f config/cartpole/impala.yaml
+rllib train -f config/cartpole/apex.yaml
+rllib train -f config/cartpole/sac.yaml
