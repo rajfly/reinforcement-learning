3c2d15d8ff2e18d0a8ee8b86ee0c2af776ef65bd
rajdeephu <rajdeephu@gmail.com>
2022-12-02 09:51:58 +0800

    fixed data read bug

--- metrics.py
+++ metrics.py
@@ -5,11 +5,10 @@ from ast import literal_eval
 from scipy.stats import iqr, scoreatpercentile
 import matplotlib.pyplot as plt
 
-# returns rewards from all episodes for a particular training run
-def get_episode_rewards(path):
+# returns rewards and timesteps for a particular training run
+def get_episode_info(path):
     df = pd.read_csv(path)
-    df['hist_stats/episode_reward'] = df['hist_stats/episode_reward'].apply(lambda x: literal_eval(x))
-    return np.hstack(df['hist_stats/episode_reward'].to_numpy())
+    return df['episode_reward_mean'].astype(float).to_numpy(), df['timesteps_total'].astype(float).to_numpy()
 
 # returns batches from numpy array
 def get_batches(arr, batch_size):
@@ -27,19 +26,22 @@ def get_normalize_range(arr):
 
 # returns the inter quartile range (iqr) from an array
 # returns mean iqr from sliding window if window_size is specified
-def get_iqr(arr, detrend=False, window_size=None):
-    norm = get_normalize_range(arr)
-    if detrend: arr = np.diff(arr)
+def get_iqr(rewards, timesteps, detrend=False, window_size=None):
+    norm = get_normalize_range(rewards)
+    if detrend: 
+        rewards = np.diff(rewards)
+        timesteps = np.diff(timesteps)
+        rewards = np.true_divide(rewards, timesteps)
     if window_size is None:
-        return iqr(arr) / norm
+        return iqr(rewards) / norm
     else:
         batch_iqr = []
-        for batch in get_batches(arr, window_size):
+        for batch in get_batches(rewards, window_size):
             batch_iqr.append(iqr(batch))
         return np.mean(batch_iqr / norm)
 
 # returns the conditional value at risk (cvar) of an array
-def get_cvar(arr, alpha=0.05, differences=False, drawdown=False):
+def get_cvar(arr, arr2, alpha=0.05, differences=False, drawdown=False):
     arr = arr / get_normalize_range(arr)
     if differences:
         arr = np.diff(arr)
@@ -63,7 +65,7 @@ if __name__ == '__main__':
     tf2_cvar_diff_rankings = []
     torch_cvar_diff_rankings = []
 
-    for exp in ['a2c', 'apex', 'dqn', 'impala', 'ppo', 'appo', 'pg']:
+    for exp in ['a2c', 'apex', 'dqn', 'impala', 'ppo', 'appo', 'pg', 'arc']:
         exp_iqr_val = []
         exp_cvar_diff_val = []
         exp_path = 'cartpole/' + exp
@@ -76,27 +78,27 @@ if __name__ == '__main__':
                 elif '=tfe_' in data_path: framework = 'tfe'
                 elif '=tf_' in data_path: framework = 'tf'
                 elif '=torch_' in data_path: framework = 'torch'
-                episode_rewards = get_episode_rewards(data_path)
-                iqr_val = get_iqr(np.copy(episode_rewards), True, 32)
-                cvar_diff = get_cvar(np.copy(episode_rewards), 0.05, True, False)
-                cvar_draw = get_cvar(np.copy(episode_rewards), 0.05, False, True)
+                episode_rewards, episode_timesteps = get_episode_info(data_path)
+                iqr_val = get_iqr(np.copy(episode_rewards), np.copy(episode_timesteps), True, 10)
+                # cvar_diff = get_cvar(np.copy(episode_rewards), np.copy(episode_timesteps), 0.05, True, False)
+                # cvar_draw = get_cvar(np.copy(episode_rewards), np.copy(episode_timesteps), 0.05, False, True)
                 exp_iqr_val.append((iqr_val, framework))
-                exp_cvar_diff_val.append((cvar_diff, framework))
+                # exp_cvar_diff_val.append((cvar_diff, framework))
         
         # calculate rankings for iqr and cvar
         exp_iqr_val.sort(key=lambda x:x[0])
-        exp_cvar_diff_val.sort(key=lambda x:x[0], reverse=True)
+        # exp_cvar_diff_val.sort(key=lambda x:x[0], reverse=True)
         for idx, x in enumerate(exp_iqr_val):
             if x[1] == 'tf2': tf2_iqr_rankings.append(idx+1)
             elif x[1] == 'tfe': tfe_iqr_rankings.append(idx+1)
             elif x[1] == 'tf': tf_iqr_rankings.append(idx+1)
             elif x[1] == 'torch': torch_iqr_rankings.append(idx+1)
 
-        for idx, x in enumerate(exp_cvar_diff_val):
-            if x[1] == 'tf2': tf2_cvar_diff_rankings.append(idx+1)
-            elif x[1] == 'tfe': tfe_cvar_diff_rankings.append(idx+1)
-            elif x[1] == 'tf': tf_cvar_diff_rankings.append(idx+1)
-            elif x[1] == 'torch': torch_cvar_diff_rankings.append(idx+1)
+        # for idx, x in enumerate(exp_cvar_diff_val):
+        #     if x[1] == 'tf2': tf2_cvar_diff_rankings.append(idx+1)
+        #     elif x[1] == 'tfe': tfe_cvar_diff_rankings.append(idx+1)
+        #     elif x[1] == 'tf': tf_cvar_diff_rankings.append(idx+1)
+        #     elif x[1] == 'torch': torch_cvar_diff_rankings.append(idx+1)
 
     # plot iqr figure
     iqr_fig_data = {
@@ -116,18 +118,18 @@ if __name__ == '__main__':
     plt.clf()
 
     # plot cvar (diff) figure
-    cvar_diff_fig_data = {
-        'TF': np.mean(tf_cvar_diff_rankings),
-        'TFE': np.mean(tfe_cvar_diff_rankings),
-        'TF2': np.mean(tf2_cvar_diff_rankings),
-        'TORCH': np.mean(torch_cvar_diff_rankings)}
+    # cvar_diff_fig_data = {
+    #     'TF': np.mean(tf_cvar_diff_rankings),
+    #     'TFE': np.mean(tfe_cvar_diff_rankings),
+    #     'TF2': np.mean(tf2_cvar_diff_rankings),
+    #     'TORCH': np.mean(torch_cvar_diff_rankings)}
     
-    plt.bar(
-        list(cvar_diff_fig_data.keys()),
-        list(cvar_diff_fig_data.values()),
-        color=('#8e98a3', '#12b5cb', '#e52592', '#f9ab00'))
+    # plt.bar(
+    #     list(cvar_diff_fig_data.keys()),
+    #     list(cvar_diff_fig_data.values()),
+    #     color=('#8e98a3', '#12b5cb', '#e52592', '#f9ab00'))
     
-    plt.ylabel('Mean Rank')
-    plt.title('Short Term Risk Across Time')
-    plt.savefig('figures/cvar_diff.png', format='png', bbox_inches="tight")
-    plt.clf()
+    # plt.ylabel('Mean Rank')
+    # plt.title('Short Term Risk Across Time')
+    # plt.savefig('figures/cvar_diff.png', format='png', bbox_inches="tight")
+    # plt.clf()
